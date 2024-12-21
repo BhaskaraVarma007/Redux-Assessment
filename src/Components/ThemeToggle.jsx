78d2f813aa48e0f8814394e27c4099efd9d867ac
BhaskaraVarma007 <alluribhaskar007@gmail.com>
2024-12-21 13:55:14 +0530

    new File uploaded

--- src/Components/ThemeToggle.jsx
+++ src/Components/ThemeToggle.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { ThemeContext } from '../context/ThemeContext';
+import { ThemeContext } from '../Context/ThemeContext';
 
 const ThemeToggle = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import { ThemeProvider } from './context/ThemeContext';
+import { ThemeProvider } from './Context/ThemeContext';
 
 ReactDOM.render(
   <React.StrictMode>
