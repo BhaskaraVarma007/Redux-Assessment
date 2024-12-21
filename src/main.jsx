27813d7f3857e193d67d3cb0e0266c764c5ce768
BhaskaraVarma007 <alluribhaskar007@gmail.com>
2024-12-21 13:20:54 +0530

    jnhtgbv

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+// import { ThemeProvider } from './Context/ThemeContext';
 import { ThemeProvider } from './context/ThemeContext';
 
 ReactDOM.render(
