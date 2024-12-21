76d9dc966f17d4d8d4226c7536c942a3705a7611
BhaskaraVarma007 <alluribhaskar007@gmail.com>
2024-12-21 13:50:36 +0530

    new uploaded

--- src/Context/ThemeContext.jsx
+++ src/Context/ThemeContext.jsx
@@ -1,17 +1,27 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+
 
 export const ThemeContext = createContext();
 
+
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light');
+ 
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
 
   const toggleTheme = () => {
-    setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme); // Save the selected theme in localStorage
   };
+
+  useEffect(() => {
+ 
+    document.body.className = theme;
+  }, [theme]);
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      <div className={theme}>{children}</div>
+      {children}
     </ThemeContext.Provider>
   );
 };
